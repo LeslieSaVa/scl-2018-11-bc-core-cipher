2f45b41f90791a583439dc1821c7a59b3057e93f
LeslieSaVa <leslie.sabattin@usach.cl>
2018-11-30 10:56:11 -0300

    Mi primer commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-/* Acá va tu código */
+function esconder() {
+document.getElementById("myDiv").style.display ="none";
+document.getElementById("myDiv1").style.display ="block";
+  }
+function cifrar(){
+  document.getElementById("myDiv2").style.display ="inline-block";
+  let offset = document.getElementById("offset").value;
+      offset=parseInt(offset);
+  let cajaTexto = document.getElementById("cajaTexto").value;
+  console.log(window.cipher.encode(offset,cajaTexto));
+  document.getElementById("myDiv2").innerHTML = window.cipher.encode(offset,cajaTexto);
+
+}
+
+function descifrar(){
+  document.getElementById("myDiv2").style.display ="inline-block";
+  let offset = document.getElementById("offset").value;
+    offset=parseInt(offset);
+  let cajaTexto = document.getElementById("cajaTexto").value;
+  console.log(window.cipher.decode(offset,cajaTexto));
+  document.getElementById("myDiv2").innerHTML = window.cipher.decode(offset,cajaTexto);
+}
