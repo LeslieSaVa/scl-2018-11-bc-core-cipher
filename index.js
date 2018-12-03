17b04c7833afea843016a2ca49519720c39ad4de
LeslieSaVa <leslie.sabattin@usach.cl>
2018-12-03 16:38:43 -0300

    commit antes de arreglar css

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-function esconder() {
-document.getElementById("myDiv").style.display ="none";
-document.getElementById("myDiv1").style.display ="block";
-  }
-function cifrar(){
+window.onload = ()=>{
+document.getElementById("botonNext").addEventListener("click",
+(evento) =>{
+  evento.preventDefault();
+  document.getElementById("myDiv").style.display ="none";
+  document.getElementById("myDiv1").style.display ="block";
+
+})
+
+document.getElementById("botonCifrar").addEventListener("click",
+(evento) => {
+  evento.preventDefault();
   document.getElementById("myDiv2").style.display ="inline-block";
+  document.getElementById("volver").style.display ="block";
   let offset = document.getElementById("offset").value;
       offset=parseInt(offset);
-  let cajaTexto = document.getElementById("cajaTexto").value;
-  console.log(window.cipher.encode(offset,cajaTexto));
-  document.getElementById("myDiv2").innerHTML = window.cipher.encode(offset,cajaTexto);
+  let boxText = document.getElementById("boxText").value;
 
-}
+  document.getElementById("myDiv2").innerHTML = window.cipher.encode(offset,boxText);
 
-function descifrar(){
+})
+
+document.getElementById("botonDescifrar").addEventListener("click",
+(evento)=> {
+  evento.preventDefault();
   document.getElementById("myDiv2").style.display ="inline-block";
+  document.getElementById("volver").style.display ="block";
   let offset = document.getElementById("offset").value;
     offset=parseInt(offset);
-  let cajaTexto = document.getElementById("cajaTexto").value;
-  console.log(window.cipher.decode(offset,cajaTexto));
-  document.getElementById("myDiv2").innerHTML = window.cipher.decode(offset,cajaTexto);
+  let boxText = document.getElementById("boxText").value;
+
+  document.getElementById("myDiv2").innerHTML = window.cipher.decode(offset,boxText);
+})
+
 }
+
+   document.getElementById("botonVolver").addEventListener("click", () => {
+       document.location = "index.html";
+   })
